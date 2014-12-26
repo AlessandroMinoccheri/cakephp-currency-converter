e51c8901496cd39e9c866b502644bb2803fe26f8
Alessandro Minoccheri <alessandro.minoccheri@gmail.com>
2014-12-26 11:17:52 +0100

    fix fixture

--- Test/Fixture/CurrencyConverterFixture.php
+++ Test/Fixture/CurrencyConverterFixture.php
@@ -1,6 +1,8 @@
 <?php
 class CurrencyConverterFixture extends CakeTestFixture {
 
+    public $name = 'CurrencyConverter';
+
     public $fields = array(
       'id'       => array('type' => 'integer', 'key' => 'primary'),
       'from' => array('type' => 'string'),
@@ -11,23 +13,6 @@ class CurrencyConverterFixture extends CakeTestFixture {
       'indexes'  => array('PRIMARY' => array('column' => 'id', 'unique' => 1))
     );
 
-    /*public $records = array(
-        array(
-          'id' => 1,
-          'from' => 'EUR',
-          'to' => 'GBP',
-          'rates' => '2',
-          'created' => '2014-03-18 10:39:23',
-          'modified' => '2014-03-18 10:39:23'
-        ),
-        array(
-          'id' => 2,
-          'from' => 'GBP',
-          'to' => 'EUR',
-          'rates' => '1',
-          'created' => '2014-03-18 10:39:23',
-          'modified' => '2014-03-18 10:39:23'
-        )
-    );*/
+    public $records = array();
 }
 ?>
