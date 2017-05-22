e8810836661981d62975b0ea15310161f334c45f
Alessandro Minoccheri <alessandro@studiomado.it>
2017-05-22 21:13:33 +0200

    fixed tests and table configuration

--- src/Model/Table/CurrencyConvertersTable.php
+++ src/Model/Table/CurrencyConvertersTable.php
@@ -3,13 +3,14 @@
 namespace CurrencyConverter\Model\Table;
 
 use Cake\ORM\Table;
+use Cake\ORM\Behavior\TimestampBehavior;
 
 class CurrencyConvertersTable extends Table
 {
 	public function initialize(array $config)
     {
+        $this->addBehavior('Timestamp');
+
         $this->table('currency_converters');
     }
 }
-
-?>
